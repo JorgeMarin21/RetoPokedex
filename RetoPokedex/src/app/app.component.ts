4dabeec2f85e53c1287093c5bd5e4bf4e65fae9c
Marin <jorge.marin@accenture.com>
2020-01-28 14:39:42 -0500

    conexion hecha

--- RetoPokedex/src/app/app.component.ts
+++ RetoPokedex/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { PokeService } from './services/poke.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'RetoPokedex';
+
+  constructor(
+    private pokeService: PokeService
+  ) {}
+
+
+  getAllPokemons() {
+    this.pokeService.getAllPokemons().subscribe(pokemons => {
+      console.log(pokemons);
+    }
+    );
+  }
+  getPokemon() {
+    this.pokeService.getPokemon('ivysaur')
+    .subscribe(pokemons => {
+      console.log(pokemons);
+    });
+  }
 }
+
--- RetoPokedex/src/app/app.module.ts
+++ RetoPokedex/src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { EncabezadoComponent } from './encabezado/encabezado.component';
+import { PokedexComponent } from './pokedex/pokedex.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EncabezadoComponent,
+    PokedexComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
